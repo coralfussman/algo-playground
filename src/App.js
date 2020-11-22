18210b18607b4b325270868e11e00181d49835a5
coralfussman <coralfussman@gmail.com>
2020-11-21 23:49:43 -0500

    Intro Component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import Palindrome from "./Palindrome";
 import TwoNumber from "./TwoNumber";
+import Intro from "./Intro";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Palindrome />
-          <TwoNumber />
-          <ValidSub />
-        </p>
+
+        <Intro />
+        <Palindrome />
+        <TwoNumber />
+        <ValidSub />
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Intro.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Intro() {
+  return (
+    <div>
+      <h2>Testing playground for all my Algos below</h2>
+      <h3> If all is well They will render results here:</h3>
+    </div>
+  );
+}
