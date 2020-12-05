75320c234f017a2aaf98637ba711f4427ee9271f
coralfussman <coralfussman@gmail.com>
2020-12-05 17:30:45 -0500

    main intro

--- src/Intro.js
+++ src/Intro.js
@@ -5,6 +5,11 @@ export default function Intro() {
     <div>
       <h2>Testing playground for all my Algos below</h2>
       <h3> If all is well They will render results here:</h3>
+
+      <br />
+      <br />
+      <br />
+      <h3> time complexity:</h3>
     </div>
   );
 }
