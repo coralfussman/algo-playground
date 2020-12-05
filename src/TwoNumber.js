f7c6ac6e92ac3cef2717e496eb8238e0b37c82bc
coralfussman <coralfussman@gmail.com>
2020-12-05 17:23:30 -0500

    Two Num Component

--- src/TwoNumber.js
+++ src/TwoNumber.js
@@ -1,9 +1,21 @@
 import React from "react";
 
 export default function TwoNumber() {
+    const twoNumberSum => (array, targetSum) {
+        array.sort((a,b) => a - b),
+          let left = 0
+          let right = array.length - 1
+          while (left < right) {
+              const currentSum = array[left] + array[right]
+              if (currentSum === targetSum) {
+                  return [array[left], array[right]]
+              }
+          }
+      }
+
   return (
     <div>
-      <h2>TBD</h2>
+      <h2>{twoNumberSum}</h2>
     </div>
   );
 }
