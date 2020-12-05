1c26129b2af9731deae63c465843e5f0116f001f
coralfussman <coralfussman@gmail.com>
2020-12-05 17:42:38 -0500

    fibonacci sequence

--- src/Fib.js
+++ src/Fib.js
@@ -19,7 +19,11 @@ export default function Fib() {
     let counter = 3;
     while (counter <= n) {
       const nextFib = lastTwo[0];
+      lastTwo[0] = lastTwo[1];
+      lastTwo[1] = nextFib;
+      counter++;
     }
+    return n > 1 ? lastTwo[1] : lastTwo[0];
   };
 
   return (
